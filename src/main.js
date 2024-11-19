9b1d41d05d09ce26a9b3495f3783dca3bc19619d
Zack Rouch <zachary.rouch@student.swic.edu>
2024-11-18 20:17:49 -0600

    Add in-class

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-// TODO: Do what must be done
+const paragraphs = window.document.querySelectorAll("p");
+
+console.log(Array.from(paragraphs).map((pTag) => pTag.innerText));
+
+const ps = Array.from(paragraphs)
+
+console.log(ps.map((pTage) => pTag.innerText));
